664af230484a84bdac39d99598c530856650f39e
Nitish Singh <91714033+singhnitish12@users.noreply.github.com>
2023-08-10 08:49:59 +0530

    Add files via upload

---
+++ WheatherApp/index.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int weight;
+    float height;
+    cin>>weight>>height;
+    int bmi = weight/sqrt(height);
+   if( bmi < 18){
+      cout << "0";
+   }
+   else if( bmi > 18 && bmi < 25){
+    cout << "1";
+   }
+    else if( bmi > 25 && bmi < 30){
+    cout << "2";
+   }
+   else if( bmi > 30 && bmi < 40){
+    cout << "3";
+   }
+   else {
+    cout << "4";
+   }
+}
